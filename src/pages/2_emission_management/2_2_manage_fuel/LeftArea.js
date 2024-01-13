a6b938f7e095f34207a41e9416df2ab911e92f47
PaulOh5 <ghks4861@gmail.com>
2024-01-13 16:44:00 +0900

    배출활동 연료 관리 페이지 추가(미완)

---
+++ src/pages/2_emission_management/2_2_manage_fuel/LeftArea.js
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import { useSetRecoilState, useRecoilValue } from "recoil";
+import { useGridApiRef } from "@mui/x-data-grid";
+import { LinearProgress } from "@mui/material";
+
+import ContentBody from "../../../components/ContentBody";
+import SubTitle from "../../../components/SubTitle";
+import CustomDataGrid from "../../../components/datagrid/CustomDataGrid.js";
+import { UserCompanyId, SelectedYear, SelectedFactoryId } from "./States";
+import { esgFetch } from "../../../components/FetchWrapper.js";
+
+const NoRowsOverlay = () => {
+  return (
+      <div style={{ textAlign: 'center', padding: '5px 0px', backgroundColor: '#808080' }}>
+          <div style={{ fontSize: '16px' }}>조회 된 사업장 정보가 없습니다.</div>
+      </div>
+  )
+}
+
+const FacilityList = () => {
+    const apiRef = useGridApiRef();
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const userCompanyId = useRecoilValue(UserCompanyId);
+    const selectedYear = useRecoilValue(SelectedYear);
+    const setSelectedFacotyId = useSetRecoilState(SelectedFactoryId);
+
+    const fetchFactories = async () => {
+        // setLoading(true);
+        const url = `/api/facilities?` + 
+            `filters[company][id]=${userCompanyId}&` +
+            `filters[company][createdAt][$gte]=${selectedYear}-01-01&` + 
+            `filters[company][createdAt][$lte]=${selectedYear}-12-31`;
+        const response = await esgFetch(url, 'GET');
+        if (response.ok) {
+            const { data: value } = await response.json();
+            const newData = value.map((v, i) => {
+                return {
+                    index: i + 1,
+                    id: v.id,
+                    name: v.attributes.name,
+                    number: '111-11-11111' // TODO: 사업자 등록번호 수정 필요
+                }
+            });
+            // setData(newData);
+        }
+    }
+
+    // 사업장 목록 조회
+    useEffect(() => {
+        fetchFactories();
+    }, [userCompanyId, selectedYear]);
+
+    // DataGrid에 데이터가 표시되면 로딩 상태 변경
+    useEffect(() => {
+        setLoading(false);
+    }, [data])
+
+    const dummyColumns = [
+        { field: 'index', headerName: 'No', flex: 1 },
+        { field: 'name', headerName: '사업장명', flex: 2 },
+        { field: 'number', headerName: '사업자번호', flex: 2 },
+    ]
+
+    return (
+        <ContentBody>
+            <SubTitle title={"사업장목록"} />
+            <CustomDataGrid
+                data={data}
+                apiRef={apiRef} 
+                columns={dummyColumns} 
+                editable={false}
+                slots={{ 
+                    noRowsOverlay: NoRowsOverlay ,
+                    loadingOverlay: LinearProgress,
+                }}
+                loading={loading}
+                disableColumnMenu={true}
+                columnHeaderHeight={40}
+                rowHeight={30}
+                autoHeight
+                pageSize={5}
+                onRowClick={(params) => setSelectedFacotyId(params.row.id)}
+            />
+        </ContentBody>
+    )
+}
+
+export default FacilityList;
--- src/routers/EmissionSource.js
+++ src/routers/EmissionSource.js
@@ -5,8 +5,8 @@ import LeftNavigation from "../components/LeftNavigation";
 import PageNotFound from "../pages/99_error/PageNotFound";
 
 const ManageWorkplace = lazy(() => import('../pages/1_emission_source/manage_workplace/ManageWorkplace'));
-const ManageProduct = lazy(() => import('../pages/1_emission_source/manage_product/ManageProduct'))
 const ManageEmissionProduct = lazy(() => import ('../pages/2_emission_management/2_1_manage_product/EmissionProductManagement'));
+const ManageEmissionFuel = lazy(() => import ('../pages/2_emission_management/2_2_manage_fuel/EmissionFuelManagement'));
 const SamplePage = lazy(() => import('../pages/sample/SamplePage'));
 
 const EmissionSource = () => {
@@ -15,8 +15,8 @@ const EmissionSource = () => {
       <LeftNavigation />
       <Suspense fallback={"loading"}>
         <Routes>
-          <Route exact path="workplace" element={<ManageWorkplace />} />
           <Route exact path="product" element={<ManageEmissionProduct/>} />
+          <Route exact path="fuel" element={<ManageEmissionFuel />} />
           <Route exact path='sample' element={<SamplePage />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
