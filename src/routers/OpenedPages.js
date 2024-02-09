6d4f06db5d99269467191b0b379d7a6fb2e32ff4
selenitegb <selenitegb@gmail.com>
2024-02-09 15:59:41 +0900

    메인페이지 바로 로그인으로

--- src/routers/OpenedPages.js
+++ src/routers/OpenedPages.js
@@ -3,9 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import PageFooter from "../components/PageFooter";
 
 const Index = lazy(() => import("../pages/0_opened/Index"));
-const Esgran = lazy(() => import("../pages/0_opened/Esgran"));
-const Tanso = lazy(() => import("../pages/0_opened/Tanso"));
-const Muni = lazy(() => import("../pages/0_opened/Muni"));
 const Login = lazy(() => import("../pages/0_opened/Login"));
 const LoginFail = lazy(() => import("../pages/0_opened/LoginFail"));
 const Unauthorized = lazy(() => import("../pages/99_error/Unauthorized"));
@@ -16,9 +13,6 @@ const OpenedPages = () => {
     <Suspense fallback={"loading"}>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route exact path="/esgran" element={<Esgran />} />
-        <Route exact path="/tanso" element={<Tanso />} />
-        <Route exact path="/muni" element={<Muni />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/loginFaile" element={<LoginFail />} />
         <Route exact path="/unauthorized" element={<Unauthorized />} />
