c323d8f95aa255c9d4bb2da4edb373eb4f6e8cca
selenitegb <selenitegb@gmail.com>
2024-03-23 20:46:57 +0900

    fix

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -44,7 +44,7 @@ const PageHeader = () => {
         </>
       ) : (
         <>
-          <HeaderItem onClick={() => navigate("/signup")}>회원가입</HeaderItem>
+          <HeaderItem style={{ cursor: 'pointer' }} onClick={() => navigate("/signup")}>회원가입</HeaderItem>
           <div style={{ width: "10px", textAlign: "center" }}>|</div>
           <HeaderItem
             style={{ cursor: 'pointer' }}
