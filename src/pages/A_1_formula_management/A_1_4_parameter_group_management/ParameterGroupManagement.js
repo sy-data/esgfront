1de9c90d225c82999271b3482e64df364631cd96
yoohyelim <yhl0519@naver.com>
2024-06-15 19:15:57 +0900

    feat: 파라미터 그룹 삭제 기능 추가

--- src/pages/A_1_formula_management/A_1_4_parameter_group_management/ParameterGroupManagement.js
+++ src/pages/A_1_formula_management/A_1_4_parameter_group_management/ParameterGroupManagement.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import {ContentWithTitie} from "../../../components/Styles";
 import ParameterGroupList from "./ParameterGroupList";
 import ParameterGroupTableTitle from "./ParameterGroupTableTitle";
+import {useGridApiRef} from "@mui/x-data-grid";
+
+const dummyData = Array.from({ length: 50 }, (_, index) => ({
+    no: index + 1,
+    id: index + 1,
+    groupId: '0001',
+    groupName: '산정식 그룹1',
+    description: '1번 그룹'
+})).reverse();
 
 const ParameterGroupManagement = () => {
+    const gridApiRef = useGridApiRef();
+
+    const [data, setData] = useState(dummyData);
+    const [selectedRow, setSelectedRow] = useState([]);
+
     return (
         <ContentWithTitie>
-            <ParameterGroupTableTitle />
-            <ParameterGroupList />
+            <ParameterGroupTableTitle setData={setData} selectedRow={selectedRow} />
+            <ParameterGroupList gridApiRef={gridApiRef} data={data} setSelectedRow={setSelectedRow} />
         </ContentWithTitie>
     )
 }
--- src/pages/A_1_formula_management/A_1_4_parameter_group_management/ParameterGroupTableTitle.js
+++ src/pages/A_1_formula_management/A_1_4_parameter_group_management/ParameterGroupTableTitle.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MenuTitle from "../../../components/MenuTitle";
-import {Button, styled} from "@mui/material";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, styled} from "@mui/material";
 
 
 const TitleButtonContainer = styled('div')({
@@ -13,16 +13,63 @@ const ButtonContainer = styled('div')({
 });
 
 
-const ParameterGroupTableTitle = () => {
+const ParameterGroupTableTitle = (props) => {
+    const { setData, selectedRow } = props;
+
+    const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+
+    const handleDeleteButton = () => {
+        setOpenDeleteDialog(true);
+    }
+
+    const handleCloseDialog = () => {
+        setOpenDeleteDialog(false);
+    }
+
     return (
-        <TitleButtonContainer>
-            <MenuTitle title={"파라미터 그룹 목록"} />
-            <ButtonContainer>
-                <Button>그룹 추가</Button>
-                <Button>삭제</Button>
-            </ButtonContainer>
-        </TitleButtonContainer>
+        <>
+            <TitleButtonContainer>
+                <MenuTitle title={"파라미터 그룹 목록"}/>
+                <ButtonContainer>
+                    <Button>그룹 추가</Button>
+                    <Button onClick={handleDeleteButton} disabled={selectedRow.length === 0}>삭제</Button>
+                </ButtonContainer>
+            </TitleButtonContainer>
+
+            <DeleteDialog
+                openDeleteDialog={openDeleteDialog}
+                handleCloseDialog={handleCloseDialog}
+                selectedRow={selectedRow}
+                setData={setData}
+            />
+        </>
     )
 };
 
 export default ParameterGroupTableTitle;
+
+const DeleteDialog = (props) => {
+    const {openDeleteDialog, handleCloseDialog, selectedRow, setData} = props;
+
+    const handleDeleteConfirmButtonClick = () => {
+        setData(prevState => prevState.filter(v => !selectedRow.includes(v.id)));
+        handleCloseDialog()
+    }
+
+    return (
+        <Dialog open={openDeleteDialog} onClose={handleCloseDialog}>
+            <DialogTitle>그룹삭제</DialogTitle>
+
+            <DialogContent>
+                <DialogContentText>
+                    선택하신 {selectedRow.length}개의 항목을 삭제 하시겠습니까?
+                </DialogContentText>
+            </DialogContent>
+
+            <DialogActions>
+                <Button onClick={handleCloseDialog}>취소</Button>
+                <Button onClick={handleDeleteConfirmButtonClick}>삭제</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
