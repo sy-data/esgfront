788de58c3480e4d23647bba2eaa7ebae19378b06
selenitegb <selenitegb@gmail.com>
2024-06-16 17:38:49 +0900

    메뉴 연결

--- src/components/LeftNavigation.js
+++ src/components/LeftNavigation.js
@@ -71,7 +71,7 @@ const LeftNavigation = () => {
         <TreeItem nodeId="9-1" label="산정식 관리">
           <TreeItem nodeId="9-1-1" label="산정식 등록" onClick={() => navigate("/admin-formula")} icon={<ArrowRight/>} />
           <TreeItem nodeId="9-1-2" label="산정식 변경이력조회" onClick={() => navigate("/admin-formula")} icon={<ArrowRight/>} />
-          <TreeItem nodeId="9-1-3" label="파라미터 그룹 관리" onClick={() => navigate("/admin-formula")} icon={<ArrowRight/>} />
+          <TreeItem nodeId="9-1-3" label="파라미터 그룹 관리" onClick={() => navigate("/admin-formula/groupManagement")} icon={<ArrowRight/>} />
           <TreeItem nodeId="9-1-4" label="파라미터 관리" onClick={() => navigate("/admin-formula")} icon={<ArrowRight/>} />
           <TreeItem nodeId="9-1-5" label="산정식 규정등급 관리" onClick={() => navigate("/admin-formula")} icon={<ArrowRight/>} />
         </TreeItem>
