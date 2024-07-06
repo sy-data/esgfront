7b560f54e211ee539bd8f661abf45aab35244c5a
leeseungnam <dltmdska01@gmail.com>
2024-07-06 15:34:02 +0900

    텍스트 입력 수정 v1.9

--- src/pages/A_1_formula_management/A_1_1Calculation_group_management/CalculationGroupManagementList.js
+++ src/pages/A_1_formula_management/A_1_1Calculation_group_management/CalculationGroupManagementList.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, memo, useCallback } from "react";
 import CustomDataGrid from "./CalculationGroupCustomDataGrid";
-import { Select, MenuItem, Checkbox } from "@mui/material";
-import TextField from "@mui/material/TextField";
+import { Select, MenuItem, Checkbox, TextField } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 // 더미 데이터 정의
@@ -20,6 +19,49 @@ export const parameterGroupListDummy = [
   { id: 12, groupId: "0012", groupName: "기타그룹" },
 ];
 
+// 별도로 관리하는 TextField 컴포넌트
+const CustomTextField = memo(({ id, value, onChange, onKeyDown, onBlur }) => {
+  const [localValue, setLocalValue] = useState(value); // 로컬 상태로 TextField 값 관리
+  const inputRef = useRef(); // input 참조
+
+  useEffect(() => {
+    setLocalValue(value); // value가 변경될 때마다 로컬 상태 업데이트
+  }, [value]);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus(); // input에 포커스 설정
+      const len = inputRef.current.value.length;
+      inputRef.current.setSelectionRange(len, len); // 커서를 텍스트 끝으로 이동
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    setLocalValue(event.target.value); // 로컬 상태 업데이트
+    onChange(event.target.value); // 부모 컴포넌트로 변경된 값 전달
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === " ") {
+      e.stopPropagation(); //스페이스바 입력 무시
+    }
+    onKeyDown(e);
+  };
+
+  return (
+    <TextField
+      inputRef={inputRef}
+      type={"text"}
+      value={localValue}
+      onChange={handleChange}
+      placeholder={"비고내용"}
+      onKeyDown={onKeyDown}
+      onBlur={onBlur}
+      onClick={(event) => event.stopPropagation()}
+    />
+  );
+});
+
 const ParameterGroupList = (props) => {
   const {
     gridApiRef,
@@ -31,14 +73,14 @@ const ParameterGroupList = (props) => {
     setEditRowId,
   } = props;
 
-  // 각 행의 편집 상태 관리
-  const [editingDescriptions, setEditingDescriptions] = useState({});
+  const [editingDescriptions, setEditingDescriptions] = useState({}); // 비고(description) 필드의 편집 상태 관리
 
   // 그룹명을 선택하는 함수
   const handleSelectGroupName = (id, value) => {
     setData((prevState) =>
       prevState.map((row) => {
         if (row.id === id) {
+          // 선택한 그룹명을 찾아서 그룹 ID와 그룹명을 업데이트
           const selectedGroup = parameterGroupListDummy.find(
             ({ groupName }) => groupName === value
           );
@@ -53,73 +95,75 @@ const ParameterGroupList = (props) => {
     );
   };
 
-  // 비고(description) 필드를 변경하는 함수
-  const handleDescriptionChange = (id, value) => {
+  // 비고(description) 필드의 변경을 처리하는 함수
+  const handleDescriptionChange = useCallback((id, value) => {
     setEditingDescriptions((prevState) => ({
       ...prevState,
       [id]: value,
     }));
-  };
+  }, []);
 
-  // 비고(description) 필드의 변경을 블러 이벤트 시 반영하는 함수
-  const handleDescriptionBlur = (id) => {
-    setData((prevState) =>
-      prevState.map((row) => {
-        if (row.id === id) {
-          return {
-            ...row,
-            description: editingDescriptions[id] || row.description,
-          };
-        }
-        return row;
-      })
-    );
-    setEditingDescriptions((prevState) => {
-      const newState = { ...prevState };
-      delete newState[id];
-      return newState;
-    });
-  };
+  // 비고(description) 필드가 블러될 때 호출되는 함수
+  const handleDescriptionBlur = useCallback(
+    (id) => {
+      setData((prevState) =>
+        prevState.map((row) => {
+          if (row.id === id) {
+            return {
+              ...row,
+              description: editingDescriptions[id] || row.description,
+            };
+          }
+          return row;
+        })
+      );
+      setEditingDescriptions((prevState) => {
+        const newState = { ...prevState };
+        delete newState[id];
+        return newState;
+      });
+    },
+    [editingDescriptions, setData]
+  );
 
-  // 엔터 키를 누를 때 비고(description) 필드를 저장하고 블러 이벤트 발생
-  const handleDescriptionKeyPress = (id, event) => {
-    if (event.key === "Enter") {
-      event.preventDefault(); // 엔터 키 기본 동작 방지
-      handleDescriptionBlur(id);
-      setEditRowId(null); // 엔터를 누르면 편집 모드 해제
-    }
-  };
+  // 엔터 키를 눌렀을 때 호출되는 함수
+  const handleDescriptionKeyPress = useCallback(
+    (id, event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleDescriptionBlur(id); // 블러 이벤트 처리
+        setEditRowId(null); // 편집 모드 해제
+      } else if (event.key === " ") {
+        event.stopPropagation(); // 스페이스바 입력 무시
+      }
+    },
+    [handleDescriptionBlur, setEditRowId]
+  );
 
-  // 행 선택 모델이 변경되었을 때의 처리 함수
-  const handleRowSelectionModelChange = (rowIds) => {
-    const isEditRow = rowIds[0] === editRowId;
+  // 행 선택 모델이 변경될 때 호출되는 함수
+  const handleRowSelectionModelChange = useCallback(
+    (rowIds) => {
+      const isEditRow = rowIds[0] === editRowId;
 
-    if (!isEditRow) {
-      setSelectedRow(rowIds);
-    }
-  };
+      if (!isEditRow) {
+        setSelectedRow(rowIds);
+      }
+    },
+    [editRowId, setSelectedRow]
+  );
 
-  // 행을 더블 클릭했을 때 편집 모드로 변경하는 함수
-  const handleRowDoubleClick = (params) => {
-    setEditRowId(params.row.id);
-    setEditingDescriptions((prevState) => ({
-      ...prevState,
-      [params.row.id]: params.row.description || "",
-    }));
-  };
-
-  useEffect(() => {
-    if (editRowId !== null) {
-      const description =
-        data.find((row) => row.id === editRowId)?.description || "";
+  // 행을 더블 클릭했을 때 호출되는 함수
+  const handleRowDoubleClick = useCallback(
+    (params) => {
+      setEditRowId(params.row.id); // 해당 행을 편집 모드로 설정
       setEditingDescriptions((prevState) => ({
         ...prevState,
-        [editRowId]: description,
+        [params.row.id]: params.row.description || "",
       }));
-    }
-  }, [editRowId, data]);
+    },
+    [setEditRowId]
+  );
 
-  // 데이터 그리드의 컬럼 정의
   const columns = [
     {
       field: "no",
@@ -183,24 +227,26 @@ const ParameterGroupList = (props) => {
         </span>
       ),
       renderCell: (params) => {
-        const isEditRow = params.row.id === editRowId;
-        const isAddRow = editRowId === -1 && params.row.id === data[0].id;
+        const isEditRow = params.row.id === editRowId; // 현재 행이 편집 모드인지 확인
+        const isAddRow = editRowId === -1 && params.row.id === data[0].id; // 현재 행이 새로 추가된 행인지 확인
         if (isEditRow || isAddRow) {
+          // 편집 모드이거나 새로 추가된 행인 경우
           return (
             <Select
-              value={params.value}
-              onChange={(event) =>
-                handleSelectGroupName(params.id, event.target.value)
+              value={params.value} // 현재 셀의 값을 선택된 값으로 설정
+              onChange={
+                (event) => handleSelectGroupName(params.id, event.target.value) // 그룹명이 변경될 때 호출되는 함수
               }
             >
               {parameterGroupListDummy.map(({ groupId, groupName }) => (
                 <MenuItem key={groupId} value={groupName}>
-                  {groupName}
+                  {groupName} {/* 각 그룹명을 선택 옵션으로 제공 */}
                 </MenuItem>
               ))}
             </Select>
           );
         } else {
+          // 편집 모드가 아닌 경우  셀의 값을 그대로 표시
           return params.value;
         }
       },
@@ -230,20 +276,16 @@ const ParameterGroupList = (props) => {
         const isAddRow = editRowId === -1 && params.row.id === data[0].id;
         if (isEditRow || isAddRow) {
           return (
-            <TextField
-              type={"text"}
+            <CustomTextField
+              id={params.id}
               value={
                 editingDescriptions[params.id] !== undefined
                   ? editingDescriptions[params.id]
                   : params.value || ""
               }
-              onChange={(event) =>
-                handleDescriptionChange(params.id, event.target.value)
-              }
-              placeholder={"비고내용"}
+              onChange={(value) => handleDescriptionChange(params.id, value)}
               onKeyDown={(event) => handleDescriptionKeyPress(params.id, event)}
               onBlur={() => handleDescriptionBlur(params.id)}
-              onClick={(event) => event.stopPropagation()}
             />
           );
         } else {
@@ -254,7 +296,6 @@ const ParameterGroupList = (props) => {
     },
   ];
 
-  // Checkbox 스타일 정의
   const CustomCheckbox = styled(Checkbox)(({ theme }) => ({
     width: 20,
     height: 20,
@@ -270,16 +311,16 @@ const ParameterGroupList = (props) => {
 
   return (
     <CustomDataGrid
-      apiRef={gridApiRef}
-      data={data}
-      columns={columns}
-      checkboxSelection
-      pageSize={15}
-      rowSelectionModel={selectedRow}
-      onRowSelectionModelChange={handleRowSelectionModelChange}
-      onRowDoubleClick={handleRowDoubleClick}
+      apiRef={gridApiRef} // DataGrid의 API를 참조할 수 있도록 설정
+      data={data} // 표시할 데이터 설정
+      columns={columns} // 정의된 컬럼 설정
+      checkboxSelection // 행 선택을 위한 체크박스 표시
+      pageSize={15} // 페이지당 행의 수 설정
+      rowSelectionModel={selectedRow} // 선택된 행 모델 설정
+      onRowSelectionModelChange={handleRowSelectionModelChange} // 행 선택 모델이 변경될 때 호출되는 함수
+      onRowDoubleClick={handleRowDoubleClick} // 행을 더블 클릭했을 때 호출되는 함수
       components={{
-        BaseCheckbox: CustomCheckbox,
+        BaseCheckbox: CustomCheckbox, // 기본 체크박스 컴포넌트를 커스텀 체크박스로 대체
       }}
     />
   );
