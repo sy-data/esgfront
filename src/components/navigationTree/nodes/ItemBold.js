a8e4d39f1fbfffc05cf68313a50a1f78ef308d10
selenitegb <selenitegb@gmail.com>
2024-07-14 19:09:11 +0900

    tree

--- src/components/navigationTree/nodes/ItemBold.js
+++ src/components/navigationTree/nodes/ItemBold.js
@@ -4,6 +4,7 @@ import chevron_item_bold_selected from "../images/chevron_item_bold_selected.svg
 
 const ItemBold = props => {
   const toggleMenu = () => {
+    console.log("ItemBold");
     props.toggle(props)
   }
   
@@ -11,16 +12,6 @@ const ItemBold = props => {
     <Box
       key={`${props.id}${props.index}`}
       tid={props.id}
-      sx={{
-        height: "56px", width: '100%',
-        padding: '11px 16px',
-        fontSize: "18px",
-        lineHeight: "27px",
-        display: 'flex', gap: '12px',
-        alignItems: 'center',
-        boxSizing: 'border-box',
-        ...(props.paddingLeft && {paddingLeft: `${props.paddingLeft+16}px`})
-      }}
       className={`treeItem treeItem-bold ${props.opened ? "opened" : ""}`}
       onClick={toggleMenu}
     >
--- src/components/navigationTree/nodes/ItemPlain.js
+++ src/components/navigationTree/nodes/ItemPlain.js
@@ -11,17 +11,7 @@ const ItemPlain = props => {
     <Box
       key={`${props.id}${props.index}`}
       tid={props.id}
-      sx={{
-        height: "56px", width: '100%',
-        padding: '11px 16px',
-        fontSize: "18px",
-        lineHeight: "27px",
-        display: 'flex', gap: '12px',
-        alignItems: 'center',
-        boxSizing: 'border-box',
-        ...(props.paddingLeft && {paddingLeft: `${props.paddingLeft}px`})
-      }}
-      className={`treeItem treeItem-bold ${props.opened ? "opened" : ""}`}
+      className={`treeItem treeItem-plain ${props.opened ? "opened" : ""}`}
       onClick={toggleMenu}
     >
       <img alt='' src={props.opened?chevron_item_selected:chevron_item} />
