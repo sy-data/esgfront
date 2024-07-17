b35cf107e24823dcb46eec30fdc9c9f23cc5fccf
leeseungnam <dltmdska01@gmail.com>
2024-07-17 20:16:47 +0900

    RegCalcFormulaMgmts 추가

--- src/pages/A_1_formula_management/A_1_2_RegistrationCalculationFormula/RegCalcFormulaMgmt.js
+++ src/pages/A_1_formula_management/A_1_2_RegistrationCalculationFormula/RegCalcFormulaMgmt.js
@@ -6,24 +6,43 @@ import {
 } from "../../../States/leftNavigation/tree";
 import NavigationTree from "./NavigationTree";
 import MenuList from "../../../MenuItems";
+import RegCalcFormulaMgmts from "./RegCalcFormulaMgmts";
 
 const Frame = () => {
   return (
     <Box
       className="frame"
       sx={{
+        display: "flex",
         width: "100%",
         bgcolor: "#f5f5f5",
         padding: 2,
         // position: "relative",
       }}
     >
-      <Typography variant="h6">산정식 목록</Typography>
-      <NavigationTree
-        items={MenuList}
-        stateAtom={treeStateAtom}
-        leafAtom={treeOpenedLeaf}
-      />
+      <Box
+        sx={{
+          width: "20%",
+          paddingRight: 2,
+          borderRight: "1px solid #e0e0e0",
+        }}
+      >
+        <Typography variant="h6">산정식 목록</Typography>
+
+        <NavigationTree
+          items={MenuList}
+          stateAtom={treeStateAtom}
+          leafAtom={treeOpenedLeaf}
+        />
+      </Box>
+      <Box
+        sx={{
+          flex: "1",
+          paddingLeft: 2,
+        }}
+      >
+        <RegCalcFormulaMgmts />
+      </Box>
     </Box>
   );
 };
