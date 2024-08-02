2967a05760f7ae5caf395c7a70e79c9d8cdebf4e
leeseungnam <dltmdska01@gmail.com>
2024-08-02 22:14:27 +0900

    CRGM css파일 분리

--- src/pages/A_1_formula_management/A_1_6_CalcRegGradeMgmt/CalcRegGradeMgmt.js
+++ src/pages/A_1_formula_management/A_1_6_CalcRegGradeMgmt/CalcRegGradeMgmt.js
@@ -3,6 +3,15 @@ import { Box, Button, Container, Typography } from "@mui/material";
 import DataTable from "./DataTable";
 import SearchBar from "./SearchBar";
 import { data as initialData } from "./data";
+import {
+  containerStyles,
+  innerBoxStyles,
+  headerStyles,
+  titleStyles,
+  buttonBoxStyles,
+  addButtonStyles,
+  deleteButtonStyles,
+} from "./styles";
 
 const CalcRegGradeMgmt = () => {
   const [selectedRows, setSelectedRows] = useState([]);
@@ -17,6 +26,7 @@ const CalcRegGradeMgmt = () => {
       usageGrade: "",
       netEmissionGrade: "",
       emissionCalcGrade: "",
+      calculationCoefficienGrade: "",
       calcDate: "",
     };
     setData([...data, newRow]);
@@ -28,115 +38,46 @@ const CalcRegGradeMgmt = () => {
     setSelectedRows([]);
   };
 
+  const handleSearch = (filters) => {
+    const filteredData = initialData.filter((row) => {
+      return (
+        (!filters.activity || row.activity === filters.activity) &&
+        (!filters.startDate ||
+          new Date(row.calcDate) >= new Date(filters.startDate)) &&
+        (!filters.endDate ||
+          new Date(row.calcDate) <= new Date(filters.endDate))
+      );
+    });
+    setData(filteredData);
+  };
+
   return (
-    <Container
-      sx={{
-        border: "10px bold #F7F8F8",
-        backgroundColor: "#F7F8F8",
-        minWidth: "1500px",
-      }}
-    >
-      <SearchBar />
-      <Box sx={{ my: 2, backgroundColor: "#FFF", borderRadius: 6 }}>
+    <Container sx={containerStyles}>
+      <SearchBar onSearch={handleSearch} />
+      <Box sx={innerBoxStyles}>
         <Box>
-          <Box>
-            <Box
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              mt={2}
-            >
-              <Typography
-                variant="h6"
-                gutterBottom
-                sx={{
-                  flexGrow: 1,
-                  color: "#000",
-                  fontFamily: "Pretendard Variable",
-                  fontSize: "18px",
-                  fontStyle: "normal",
-                  fontWeight: "700",
-                  lineHeight: "150%" /* 27px */,
-                  letterSpacing: "-0.36px",
-                  marginTop: 1,
-                  marginLeft: 3,
-                }}
+          <Box sx={headerStyles}>
+            <Typography variant="h6" gutterBottom sx={titleStyles}>
+              산정식규정등급 목록
+            </Typography>
+            <Box sx={buttonBoxStyles}>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={addButtonStyles}
+                onClick={handleAddRow}
               >
-                산정식규정등급 목록
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  gap: "10px",
-                }}
+                추가
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                disabled={selectedRows.length === 0}
+                sx={deleteButtonStyles(selectedRows.length)}
+                onClick={handleDelete}
               >
-                <Button
-                  variant="contained"
-                  color="primary"
-                  sx={{
-                    minWidth: "100px",
-                    display: "flex",
-                    height: "40px",
-                    padding: "10px 16px",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: "10px",
-                    borderRadius: "8px",
-                    background: "var(--Primary-Primary, #00CD9B)",
-                    color: "var(--Gray-fff, #FFF)",
-                    textAlign: "center",
-                    fontFamily: "Pretendard Variable",
-                    fontSize: "14px",
-                    fontStyle: "normal",
-                    fontWeight: "700",
-                    lineHeight: "150%" /* 21px */,
-                    letterSpacing: "-0.28px",
-                    marginTop: 2,
-                  }}
-                  onClick={handleAddRow}
-                >
-                  추가
-                </Button>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  disabled={selectedRows.length === 0}
-                  sx={{
-                    marginTop: 2,
-                    minWidth: "100px",
-                    marginRight: "25px",
-                    display: "flex",
-                    height: 40,
-                    padding: "10px 16px",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: "8px",
-                    color:
-                      selectedRows.length > 0
-                        ? "var(--Gray-fff, #FFF)"
-                        : "var(--Gray-ccc, #CCC)",
-                    border:
-                      selectedRows.length > 0
-                        ? "1px solid var(--Primary-Primary, #00CD9B)"
-                        : "1px solid var(--Gray-eee, #EEE)",
-                    background:
-                      selectedRows.length > 0
-                        ? "var(--Primary-Primary, #00CD9B)"
-                        : "var(--Gray-fff, #FFF)",
-                    textAlign: "center",
-                    fontFamily: "Pretendard Variable",
-                    fontSize: "14px",
-                    fontStyle: "normal",
-                    fontWeight: "700",
-                    lineHeight: "150%" /* 21px */,
-                    letterSpacing: "-0.28px",
-                  }}
-                  onClick={handleDelete}
-                >
-                  삭제
-                </Button>
-              </Box>
+                삭제
+              </Button>
             </Box>
           </Box>
           <DataTable
--- src/pages/A_1_formula_management/A_1_6_CalcRegGradeMgmt/styles.js
+++ src/pages/A_1_formula_management/A_1_6_CalcRegGradeMgmt/styles.js
@@ -48,3 +48,91 @@ export const inactiveButtonStyles = {
   lineHeight: "150%", // 21px
   letterSpacing: "-0.28px",
 };
+
+export const containerStyles = {
+  border: "10px bold #F7F8F8",
+  backgroundColor: "#F7F8F8",
+  minWidth: "1500px",
+};
+
+export const innerBoxStyles = {
+  my: 2,
+  backgroundColor: "#FFF",
+  borderRadius: 6,
+};
+
+export const headerStyles = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  mt: 2,
+};
+
+export const titleStyles = {
+  flexGrow: 1,
+  color: "#000",
+  fontFamily: "Pretendard Variable",
+  fontSize: "18px",
+  fontStyle: "normal",
+  fontWeight: "700",
+  lineHeight: "150%",
+  letterSpacing: "-0.36px",
+  marginTop: 1,
+  marginLeft: 3,
+};
+
+export const buttonBoxStyles = {
+  display: "flex",
+  flexDirection: "row",
+  gap: "10px",
+};
+
+export const addButtonStyles = {
+  minWidth: "100px",
+  display: "flex",
+  height: "40px",
+  padding: "10px 16px",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "10px",
+  borderRadius: "8px",
+  background: "var(--Primary-Primary, #00CD9B)",
+  color: "var(--Gray-fff, #FFF)",
+  textAlign: "center",
+  fontFamily: "Pretendard Variable",
+  fontSize: "14px",
+  fontStyle: "normal",
+  fontWeight: "700",
+  lineHeight: "150%",
+  letterSpacing: "-0.28px",
+  marginTop: 2,
+};
+
+export const deleteButtonStyles = (selectedRowsLength) => ({
+  marginTop: 2,
+  minWidth: "100px",
+  marginRight: "25px",
+  display: "flex",
+  height: 40,
+  padding: "10px 16px",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "8px",
+  color:
+    selectedRowsLength > 0 ? "var(--Gray-fff, #FFF)" : "var(--Gray-ccc, #CCC)",
+  border:
+    selectedRowsLength > 0
+      ? "1px solid var(--Primary-Primary, #00CD9B)"
+      : "1px solid var(--Gray-eee, #EEE)",
+  background:
+    selectedRowsLength > 0
+      ? "var(--Primary-Primary, #00CD9B)"
+      : "var(--Gray-fff, #FFF)",
+  textAlign: "center",
+  fontFamily: "Pretendard Variable",
+  fontSize: "14px",
+  fontStyle: "normal",
+  fontWeight: "700",
+  lineHeight: "150%",
+  letterSpacing: "-0.28px",
+});
