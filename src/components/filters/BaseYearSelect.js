5bc649281855c39ce52311c09cc9a12a39db89fb
selenitegb <selenitegb@gmail.com>
2024-11-03 16:18:09 +0900

    1-2 디자인

--- src/components/filters/BaseYearSelect.js
+++ src/components/filters/BaseYearSelect.js
@@ -2,6 +2,8 @@ import { useState, useImperativeHandle, forwardRef } from "react";
 import { FilterContainer, FilterLabel, FilterSelect } from "../Styles";
 import MenuItem from "@mui/material/MenuItem";
 import SvgIcon from "@mui/material/SvgIcon";
+import Select from "@mui/material/Select";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 /**
  * 1900년부터 올해까지 선택가능한 기준년도 select입니다. 
@@ -40,19 +42,24 @@ const BaseYearSelect = (props, ref) => {
   }));
   
   return (
-    <FilterContainer>
-      <FilterLabel>기준년도</FilterLabel>
-      <FilterSelect
-        value={baseYear}
-        onChange={handleChange}
-        IconComponent={(props) => (
-          <SvgIcon width="20" height="20" viewBox="0 0 20 20" fill="none" {...props}>
-            <path d="M10 13.75L3.75 7.5L4.625 6.625L10 12L15.375 6.625L16.25 7.5L10 13.75Z" fill="#111111"/>
-          </SvgIcon>
-        )}>
-        {arrayYear.map(a => <MenuItem key={'by'+a} value={a}>{a}</MenuItem>)}
-      </FilterSelect>
-    </FilterContainer>
+    <Select value={baseYear} size="small" IconComponent={ExpandMoreIcon} sx={{width: "width" in props?props.width:338}} >
+      {arrayYear.map(a => <MenuItem key={'by'+a} value={a}>{a}</MenuItem>)}
+      {/* <MenuItem value={1}>사용</MenuItem>
+      <MenuItem value={0}>미사용</MenuItem> */}
+    </Select>
+    // <FilterContainer>
+    //   <FilterLabel>기준년도</FilterLabel>
+    //   <FilterSelect
+    //     value={baseYear}
+    //     onChange={handleChange}
+    //     IconComponent={(props) => (
+    //       <SvgIcon width="20" height="20" viewBox="0 0 20 20" fill="none" {...props}>
+    //         <path d="M10 13.75L3.75 7.5L4.625 6.625L10 12L15.375 6.625L16.25 7.5L10 13.75Z" fill="#111111"/>
+    //       </SvgIcon>
+    //     )}>
+    //     {arrayYear.map(a => <MenuItem key={'by'+a} value={a}>{a}</MenuItem>)}
+    //   </FilterSelect>
+    // </FilterContainer>
   )
 }
 
--- src/pages/1_facility_information/1_2_manage_facility/ManageFacility.js
+++ src/pages/1_facility_information/1_2_manage_facility/ManageFacility.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useResetRecoilState, useSetRecoilState } from "recoil";
+import { headerTitleAtom } from "../../../States/header/Title";
 
 import ChevronRight from "@mui/icons-material/ChevronRight";
-import { Button } from "@mui/material";
+import { Button, FormControlLabel, Checkbox } from "@mui/material";
 
 import { ContentWithTitie, FilterBlock, FilterLine, SearchButtonContainer } from "../../../components/Styles";
 import SplitArea from "../../../components/SplitArea";
@@ -11,6 +12,10 @@ import BaseYearSelect from "../../../components/filters/BaseYearSelect";
 import { SelectedYear, SelectedFactoryId } from "./States";
 import LeftFacility from "./LeftFacility";
 import RightFacility from "./RightFacility";
+import ContentBody from "../../../components/ContentBody";
+import SubTitle from "../../../components/SubTitle";
+import { DataGrid } from "@mui/x-data-grid";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 
 const ManageFacility = () => {
@@ -21,19 +26,66 @@ const ManageFacility = () => {
   const resetSelectedFacotyId = useSetRecoilState(SelectedFactoryId);
 
   const handleSearchButtonClick = () => setSelectedYear(baseYearRef.current.baseYear);
+  
+  const setHeaderTitle = useSetRecoilState(headerTitleAtom);
 
   React.useEffect(() => {
+    setHeaderTitle("시설정보관리");
     return () => {
       resetSelectedYear();
       resetSelectedFacotyId();
     };
   }, []);
+  
+  const columns = [
+    { field: "id", headerName: "No", sortable: false, renderHeader: () => <FormControlLabel control={<Checkbox />} label="ID" />},
+    { field: 'place_name', headerName: '사업장명', width: 300 },
+    { field: 'facility_name', headerName: '시설명', width: 330 },
+    { field: 'facility', headerName: '배출시설', width: 330 },
+    { field: 'size', headerName: '규모' },
+    { field: 'report_target', headerName: '정부보고대상' },
+    { field: 'append_date', headerName: '시설증설일' },
+  ];
+  const rows = [];
+  const NoRowsOverlay = () => {
+  return (
+    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%", padding: '5px 0px', backgroundColor: '#FFFFFF' }}>
+      <div style={{ height: "20px", display: "flex", alignItems: "center", gap: "4px" }}>
+          <InfoOutlinedIcon sx={{size: "18px", color: "#757575"}} />
+          <div style={{ height: "20px", display: "flex", alignItems: "center", color: "#757575" }}>조회된 사업장정보가 없습니다.</div>
+        </div>
+    </div>
+  )
+}
+
 
   return (
-    <ContentWithTitie style={{ backgroundColor: "#AAAAAA" }}>
-      <MenuTitle title={<div style={{ display: "flex", alignItems: "center" }}>시설정보관리 <ChevronRight sx={{ fontSize: 40 }} /> 사업장 시설정보 관리</div>} />
+    <div style={{ backgroundColor: "#eaeaea", width: "calc(100% - 236px)", height: "100%", padding: "24px", boxSizing: "border-box",
+      display: "flex", flexDirection: "column", gap: "24px"
+     }}>
+      {/* <MenuTitle title={<div style={{ display: "flex", alignItems: "center" }}>시설정보관리 <ChevronRight sx={{ fontSize: 40 }} /> 사업장 시설정보 관리</div>} /> */}
 
-      <FilterBlock>
+      <div style={{display: "flex", gap: "6px"}}>
+        <BaseYearSelect ref={baseYearRef} />
+        <Button variant="btnDisabled" sx={{width: 114}} onClick={handleSearchButtonClick}>검색</Button>
+      </div>
+      <ContentBody padding={"24px"} gap="16px">
+        <SubTitle title={"시설 정보"}>
+          <div style={{display: "flex", gap: "8px"}}>
+            <Button sx={{width: "122px", fontSize: "14px"}} variant="btnActive">신규 등록</Button>
+            <Button sx={{width: "122px", fontSize: "14px"}} variant="btnInit">삭제</Button>
+          </div>
+        </SubTitle>
+        <DataGrid
+          columns={columns}
+          rows={rows}
+          slots={{
+            noRowsOverlay: NoRowsOverlay,
+            // loadingOverlay: LinearProgress,
+          }}
+        />
+      </ContentBody>
+      {/* <FilterBlock>
         <FilterLine>
           <BaseYearSelect ref={baseYearRef} />
         </FilterLine>
@@ -46,8 +98,8 @@ const ManageFacility = () => {
       <SplitArea>
         <LeftFacility />
         <RightFacility />
-      </SplitArea>
-    </ContentWithTitie>
+      </SplitArea> */}
+    </div>
   )
 }
 
